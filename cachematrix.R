6f4360930d320131f8ad0e72868a32603333a7cb
veritaem <dcharris776@gmail.com>
2020-08-17 14:08:36 -0400

    created a function for solving of matrix inversion with caching for improved performance

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## create a function that inverts an invertible matrix 
+## and caches the result to avoid recomput
 
-## Write a short comment describing this function
+## caching function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## the inverted mat will go here
+  n<-NULL
+  
+  ## makes a new spot for us with the matrix and a null spot for its inverse
+  set<-function(y){
+    x<<-y
+    n<<-NULL
+  }
+  ## get returns the x we have
+  get<-function(){x}
+  
+  ## setinverse takes in the function 'solve', and returns n 
+  ## as equal to that solved matrix
+  setinverse<-function(solve) n<<-solve
+  
+  ##getinverse returns the inverse we have on hand
+  getinverse<-function(){n}
+    
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## inversion.  If inversion already cached return instead.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## gets the inverse if it exists
+    q<-x$getinverse()
+    if (!is.null(q)){
+      message("pre-cached")
+      return(q)
+    }
+    
+    # if not, get, then solve x, and set its inverse in the cache
+    d<-x$get()
+    S<-solve(d,...)
+    x$setinverse(S)
+    S
 }
